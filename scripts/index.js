db41116a504be3a27fa962157dae8fcad259cde5
Smallwhitelight <smallwhitelight28@gmail.com>
2023-02-15 05:01:25 +0200

    modified bags

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@ const container = document.querySelector('.container')
 const check = document.querySelector('.check-b')
 
 
+
 check.addEventListener('click',()=>{
         container.classList.toggle('whiteToblack')
         container.classList.toggle('blackTowhite')
+
 })
